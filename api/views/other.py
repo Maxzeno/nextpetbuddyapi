78b8405c6896c90c5533ba53ea342e1dd9232211
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-05-29 02:37:19 +0100

    removed auth from some endpoints and made some minor change

--- api/views/other.py
+++ api/views/other.py
@@ -4,10 +4,14 @@ from api.views import serializers
 
 
 class ContactUsCreateView(generics.CreateAPIView):
+    authentication_classes = []
+    permission_classes = []
     queryset = models.ContactUs.objects.all()
     serializer_class = serializers.ContactUsSerializer
     
 
 class EmailCreateView(generics.CreateAPIView):
+    authentication_classes = []
+    permission_classes = []
     queryset = models.Email.objects.all()
     serializer_class = serializers.EmailSerializer
