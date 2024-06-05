31bf42d06a23d4f67419b91a1f7f3ee3d27e8bad
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-06-06 00:05:53 +0100

    worked on orders

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from api.models import Animal, Breed, Pet
 
 # Create your tests here.
+
+class AnimalModelTest(TestCase):
+    def setUp(self):
+        pet = Pet.objects.create(name="dog pet")
+        breed = Breed.objects.create(name="dog breed", pet=pet)
+        Animal.objects.create(name="dog", price=1233.45, breed=breed)
+        
+    def test_model_update(self):
+        animal_len = Animal.objects.count()
+        self.assertEqual(animal_len, 1)
